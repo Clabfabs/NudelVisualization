dd22780044c62b420dc8c08e5132f80499b66735
rebplu <rebekka.pluess@gmail.com>
2014-11-02 20:09:17 +0100

    Configuration-Klasse: Instanzvariablen und Add-Methoden, Body von weiteren Methoden.

--- src/com/example/nudelvisualization/client/Configuration.java
+++ src/com/example/nudelvisualization/client/Configuration.java
@@ -1,5 +1,39 @@
 package com.example.nudelvisualization.client;
 
-public class Configuration {
+import java.util.ArrayList;
 
+public class Configuration {
+	
+	private ArrayList<Area> selectedArea = new ArrayList<Area>();
+	private ArrayList<Item> selectedItems = new ArrayList<Item>();
+	private ArrayList<Year> selectedYears = new ArrayList<Year>();
+	private ArrayList<DataSeries> selectedDataSeries = new ArrayList<DataSeries>();
+
+
+
+public void addArea(Area area){
+	selectedArea.add(area);	
+}
+
+public void addItem(Item item){
+	selectedItems.add(item);	
+}
+
+public void addYear(Year year){
+	selectedYears.add(year);
+}
+
+public void addDataSeries(DataSeries dataSerie){
+	selectedDataSeries.add(dataSerie);
+}
+
+//to be done
+public void loadConfig(){
+	
+}
+
+//to be done
+public void saveConfig(){
+	
+}
 }
