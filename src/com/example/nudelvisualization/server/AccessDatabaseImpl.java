673244173c48eea5290e8fb31e856c022b68c6ec
EineNudel <clemmer90@gmail.com>
2014-11-04 17:26:30 +0100

    added getSelectedRows method, not yet finished though. havent implemented the filter function yet.

--- src/com/example/nudelvisualization/server/AccessDatabaseImpl.java
+++ src/com/example/nudelvisualization/server/AccessDatabaseImpl.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import com.example.nudelvisualization.client.AccessDatabase;
+import com.example.nudelvisualization.client.Configuration;
 import com.example.nudelvisualization.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -95,7 +96,89 @@ public class AccessDatabaseImpl extends RemoteServiceServlet implements AccessDa
 		}
 		return data;
 	}
+	
+	public String[][] getSelectedRows(Configuration config) {
 
+		/*// TODO Verify that the input is valid. Example Code:
+		if (!FieldVerifier.isValidName(input)) {
+	      // If the input is not valid, throw an IllegalArgumentException back to
+	      // the client.
+	      throw new IllegalArgumentException(
+	          "Name must be at least 4 characters long");
+	    }*/
+		
+		// Stuff we need for csv import
+		String csvFile = "data/production1990-2011.csv";
+		BufferedReader br = null;
+		String cvsSplitBy = ",";
+
+		// The data we want to fill
+		String[] dataRow = null;
+		ArrayList<String[]> dataList = new ArrayList<String[]>();
+		String[] firstLine = null;
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			String line = br.readLine();
+			
+			// workaround to get rid of the first 3 annoying characters -> to be improved! :-)
+			String line2 = line.substring(3);
+
+			if (line2 != null) {
+				 firstLine = line2.split(cvsSplitBy);
+				dataList.add(firstLine);
+				
+				int i = 1;
+				
+				//To do: better while loop.
+				while (i < 10000) {
+					line = br.readLine();
+					if (line == null) break;
+					
+					String[] cells = line.split(cvsSplitBy);
+					
+					// Filter
+					
+				//	for(int f=0; f<config.size.selectedyear(), f++){}
+				//		if(cells[8].equals(config.selectedyear(f)){}						
+					{
+						dataRow = new String[firstLine.length];
+						dataRow = line.split(cvsSplitBy);
+						dataList.add(dataRow);
+						i++;
+					}
+				} 
+				
+				
+			} else {
+				System.out.println("First line failure");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		String[][] data = new String[dataList.size()][firstLine.length];
+		
+		//converts ArrayList<String[]> into String[][]
+		for(int j=0; j<dataList.size(); j++){
+			for(int k=0; k<firstLine.length; k++){
+				data[j][k] = dataList.get(j)[k];
+				
+			}
+		
+		}
+		return data;
+	}
+	
 	/**
 	 * Escape an html string. Escaping data received from the client helps to
 	 * prevent cross-site script vulnerabilities.
