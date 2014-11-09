76cc099ec07f30545ad15f37033d4aed32c4b99f
Nadine <nadine.piv@gmail.com>
2014-11-09 14:18:50 +0100

    Test for Item methods

--- src/com/example/nudelvisualization/client/Item.java
+++ src/com/example/nudelvisualization/client/Item.java
@@ -26,17 +26,10 @@ public class Item implements FilterItem {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getID() {
 		return ID;
 	}
 
-	public void setID(String iD) {
-		ID = iD;
-	}
 	
 
 }
---
+++ test/com/example/nudelvisualization/client/ItemTest.java
@@ -0,0 +1,18 @@
+package com.example.nudelvisualization.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTest {
+
+	@Test
+	public void test() {
+		Item itemTester = new Item("1", "Crops");
+		assertEquals("1", itemTester.getID());
+		assertEquals("Crops", itemTester.getName());
+		assertEquals(false, itemTester.isActive());
+		itemTester.setActive(true);
+		assertEquals(true, itemTester.isActive());
+	}
+}
