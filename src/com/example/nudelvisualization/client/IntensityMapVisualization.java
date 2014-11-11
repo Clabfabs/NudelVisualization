1dc5a36ed5983c9884ca6b3cb86d2847fbf2d1ce
rebplu <rebekka.pluess@gmail.com>
2014-11-11 19:52:03 +0100

    IntensityMapVisualization-Body

---
+++ src/com/example/nudelvisualization/client/IntensityMapVisualization.java
@@ -0,0 +1,17 @@
+package com.example.nudelvisualization.client;
+
+
+public class IntensityMapVisualization extends Visualization {
+
+	public IntensityMapVisualization(Configuration config) {
+		super(config);
+	}
+
+	@Override
+	public void draw() {
+	
+		
+	}
+	
+	
+}
