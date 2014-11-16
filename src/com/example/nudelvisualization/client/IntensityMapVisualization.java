63f8fd4d46ad06983bc667deca911682a14f258b
rebplu <rebekka.pluess@gmail.com>
2014-11-16 22:49:46 +0100

    Erste Auswahl für year kann getroffen werden in der Intensity Map

--- src/com/example/nudelvisualization/client/IntensityMapVisualization.java
+++ src/com/example/nudelvisualization/client/IntensityMapVisualization.java
@@ -3,6 +3,10 @@ package com.example.nudelvisualization.client;
 
 
 
+import java.util.Arrays;
+import java.util.Hashtable;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
@@ -29,66 +33,86 @@ import com.google.gwt.visualization.client.visualizations.corechart.CoreChart;
 public class IntensityMapVisualization extends Visualization {
 
 	// GWT module entry point method.
-
-
+	private final AccessDatabaseAsync dataAccessSocket = GWT.create(AccessDatabase.class);	
+	//Hashtable<String, String> IsoCodes = new Hashtable<String, String>();
 	public IntensityMapVisualization(Configuration config) {
 		super(config);
+		//setIsoCodes();
 	}
-
+	/*private void setIsoCodes(){
+		IsoCodes.put("1", "AM");
+		IsoCodes.put("2", "AF");
+		IsoCodes.put("3", "AL");
+		IsoCodes.put("4", "DZ");
+		System.out.println(IsoCodes.get("1"));
+	}*/
 	@Override
 	public void draw() {
-		VisualizationUtils.loadVisualizationApi(
-				new Runnable() {
-					public void run() {
-						drawMap();
-					}
-				}, AnnotatedTimeLine.PACKAGE, CoreChart.PACKAGE,
-				Gauge.PACKAGE, GeoMap.PACKAGE, ImageChart.PACKAGE,
-				ImageLineChart.PACKAGE, ImageAreaChart.PACKAGE, ImageBarChart.PACKAGE,
-				ImagePieChart.PACKAGE, IntensityMap.PACKAGE,
-				MapVisualization.PACKAGE, MotionChart.PACKAGE, OrgChart.PACKAGE,Table.PACKAGE,
-				ImageSparklineChart.PACKAGE);
-
+		dataAccessSocket.getSelectedRows(configToVisualize, new CallbackHandler());
 	}
 
-	private void drawMap() {
+	
+	
+	private class CallbackHandler implements AsyncCallback<String[][]>{
 
-		IntensityMap.Options options = IntensityMap.Options.create();
-		options.setRegion(IntensityMap.Region.EUROPE);
+		public void onFailure(Throwable caught) {
+			System.out.println("Communication with server failed");
+		}
+
+		public void onSuccess(final String[][] result) {
+
+		    VisualizationUtils.loadVisualizationApi(
+		    		new Runnable() {
+		    			public void run() {
+		    						IntensityMap.Options options = IntensityMap.Options.create();
+		    						options.setRegion(IntensityMap.Region.WORLD);
+		    						// 2= areacode, 3 = land, 5 = dataserie, 7=item, 8=year, 10 =value
+		    						DataTable data = DataTable.create();
+		    						data.addColumn(ColumnType.STRING, "Country");
+		    						//data.addColumn(ColumnType.NUMBER, "Import");
+		    						//data.addColumn(ColumnType.NUMBER, "Export");
+		    						data.addColumn(ColumnType.NUMBER, result[1][5]);
+		    						data.addColumn(ColumnType.NUMBER, result[1][8]);
+		    						data.addRows(5);//Anzahl Länder
+		    						data.setValue(0, 0, "GB");//(Land 1 2etc. , 0= Lädercode 1=import 2 = export 3= production, Wert)
+		    						data.setValue(0, 1, 0);
+		    						data.setValue(0, 2, 200);
+		    						//data.setValue(0, 3 ,500);
+		    						data.setValue(1, 0 ,"FR");
+		    						data.setValue(1, 1, 50);
+		    						data.setValue(1, 2, 300);
+		    						//data.setValue(1, 3 ,100);
+		    						data.setValue(2, 0, "CH");
+		    						data.setValue(2, 1, 250);
+		    						data.setValue(2, 2, 500);
+		    						//data.setValue(2, 3 , 800);
+		    						data.setValue(3, 0, "US");
+		    						data.setValue(3, 1, 40);
+		    						data.setValue(3, 2, 350);
+		    						//data.setValue(3, 3 ,100);
+		    						data.setValue(4, 0, "RU");
+		    						data.setValue(4, 1, 30);
+		    						data.setValue(4, 2, 10);
+		    						//data.setValue(4, 3 ,500);
+		    						
+		    						IntensityMap widget = new IntensityMap(data, options);
+		    						RootPanel.get("visualizationContainer").clear();
+		    						RootPanel.get("visualizationContainer").add(widget);
+		    					}
+		    				}, AnnotatedTimeLine.PACKAGE, CoreChart.PACKAGE,
+		    				Gauge.PACKAGE, GeoMap.PACKAGE, ImageChart.PACKAGE,
+		    				ImageLineChart.PACKAGE, ImageAreaChart.PACKAGE, ImageBarChart.PACKAGE,
+		    				ImagePieChart.PACKAGE, IntensityMap.PACKAGE,
+		    				MapVisualization.PACKAGE, MotionChart.PACKAGE, OrgChart.PACKAGE,Table.PACKAGE,
+		    				ImageSparklineChart.PACKAGE);		
+		    		
+		    	}
+		    }
+		    	   
+		
+	}
 		
 
-		DataTable data = DataTable.create();
-		data.addColumn(ColumnType.STRING, "Country");
-		data.addColumn(ColumnType.NUMBER, "Import");
-		data.addColumn(ColumnType.NUMBER, "Export");
-		data.addColumn(ColumnType.NUMBER, "Production");
-		data.addRows(5);//Anzahl Länder
-		data.setValue(0, 0, "GB");//(Land 1 2etc. , 0= Lädercode 1=import 2 = export 3= production, Wert)
-		data.setValue(0, 1, 0);
-		data.setValue(0, 2, 200);
-		data.setValue(0, 3 ,500);
-		data.setValue(1, 0 ,"FR");
-		data.setValue(1, 1, 50);
-		data.setValue(1, 2, 300);
-		data.setValue(1, 3 ,100);
-		data.setValue(2, 0, "CH");
-		data.setValue(2, 1, 250);
-		data.setValue(2, 2, 500);
-		data.setValue(2, 3 , 800);
-		data.setValue(3, 0, "US");
-		data.setValue(3, 1, 40);
-		data.setValue(3, 2, 350);
-		data.setValue(3, 3 ,100);
-		data.setValue(4, 0, "RU");
-		data.setValue(4, 1, 30);
-		data.setValue(4, 2, 10);
-		data.setValue(4, 3 ,500);
-		
-		IntensityMap widget = new IntensityMap(data, options);
-		RootPanel.get("visualizationContainer").clear();
-		RootPanel.get("visualizationContainer").add(widget);
-	}	
-}
 
 
 
