e212bfc8151286632fdc481001e14080cd2ba16c
Alexandre <Alexandre@dhcp-wlan-uzh-89-206-77-227.uzh.ch>
2014-11-24 16:35:54 +0100

    -kleinständerungen

---
+++ test/com/example/nudelvisualization/client/LineChartTest.java
@@ -0,0 +1,18 @@
+package com.example.nudelvisualization.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LineChartTest {
+	
+	/* JUnittest does not work because all methods are accessing data from the server.
+	 * We cannot simulate those circumstances.
+	 */ 
+	
+	@Test
+	public void test() {
+		fail("Does not work.");
+	}
+
+}
