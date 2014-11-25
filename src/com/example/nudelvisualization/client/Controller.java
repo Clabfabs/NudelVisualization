0193a9ba078e758978e7c53cfe5a8f925c4747b4
clabs <islerfab@gmail.com>
2014-11-25 19:30:28 +0100

    added bootstrap support and some basic redesign

--- src/com/example/nudelvisualization/client/Controller.java
+++ src/com/example/nudelvisualization/client/Controller.java
@@ -3,11 +3,17 @@ package com.example.nudelvisualization.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -121,19 +127,68 @@ public class Controller implements EntryPoint {
 	    buttons.add(buttonColumnChart);
 	    buttons.add(buttonLineChart);
 	    buttons.add(buttonGeoMap);
+	    buttons.addStyleName("buttonPanel");
+	    
+/*	    VerticalPanel upload = new VerticalPanel();
+	    // Create a FormPanel and point it at a service.
+	    final FormPanel form = new FormPanel();
+
+	    // Because we're going to add a FileUpload widget, we'll need to set the
+	    // form to use the POST method, and multipart MIME encoding.
+	    form.setEncoding(FormPanel.ENCODING_MULTIPART);
+	    form.setMethod(FormPanel.METHOD_POST);
+
+	    // Create a panel to hold all of the form widgets.
+	    VerticalPanel panel = new VerticalPanel();
+	    form.setWidget(panel);
+
+	    // Create a FileUpload widget.
+	    final FileUpload upload = new FileUpload();
+	    upload.setName("uploadFormElement");
+	    panel.add(upload);
+
+	    // Add a 'submit' button.
+	    panel.add(new Button("Submit", new ClickHandler() {
+	    	public void onClick(ClickEvent event) {
+	    		form.submit();
+	    	}
+	    }));
+
+    // Add an event handler to the form.
+    form.addSubmitHandler(new FormPanel.SubmitHandler() {
+      public void onSubmit(SubmitEvent event) {
+        // This event is fired just before the form is submitted. We can take
+        // this opportunity to perform validation.
+    	  System.out.println(upload.getFilename());
+      }
+    });
+    form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+      public void onSubmitComplete(SubmitCompleteEvent event) {
+    	  System.out.println("Test2");
+
+        // When the form submission is successfully completed, this event is
+        // fired. Assuming the service returned a response of type text/html,
+        // we can get the result text here (see the FormPanel documentation for
+        // further explanation).
+        Window.alert(event.getResults());
+      }
+    });*/
+
+	    
 	    filterHorizontalPanel.add(gridYear);
 	    filterHorizontalPanel.add(lbArea);
 	    filterHorizontalPanel.add(lbYear);
 	    filterHorizontalPanel.add(lbDataSeries);
 	    filterHorizontalPanel.add(lbItems);
 	    filterHorizontalPanel.add(buttons);
-	    
+	   // filterHorizontalPanel.add(panel);
+
 	    RootPanel.get("filterContainer").add(filterHorizontalPanel);	    	    
 	}
 	
 	private boolean isValidInput() {
 		if (config.getSelectedAreaList().isEmpty()
-				// || config.getSelectedDataSeriesList()
+				|| config.getSelectedDataSeriesList().isEmpty()
 				|| config.getSelectedItemsList().isEmpty()
 				|| config.getSelectedYearsList().isEmpty()) {
 			return false;
