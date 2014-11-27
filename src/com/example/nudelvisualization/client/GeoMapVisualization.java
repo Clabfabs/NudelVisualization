f3b3bd16b7787ba0cd2841e986494246e05cea5a
rebplu <rebekka.pluess@gmail.com>
2014-11-27 12:10:15 +0100

    Update Geomap (Beschriftung)

--- src/com/example/nudelvisualization/client/GeoMapVisualization.java
+++ src/com/example/nudelvisualization/client/GeoMapVisualization.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
 import com.google.gwt.visualization.client.VisualizationUtils;
@@ -19,7 +19,6 @@ import com.google.gwt.visualization.client.visualizations.ImageLineChart;
 import com.google.gwt.visualization.client.visualizations.ImagePieChart;
 import com.google.gwt.visualization.client.visualizations.ImageSparklineChart;
 import com.google.gwt.visualization.client.visualizations.IntensityMap;
-import com.google.gwt.visualization.client.visualizations.IntensityMap.Region;
 import com.google.gwt.visualization.client.visualizations.MapVisualization;
 import com.google.gwt.visualization.client.visualizations.MotionChart;
 import com.google.gwt.visualization.client.visualizations.OrgChart;
@@ -31,8 +30,8 @@ public class GeoMapVisualization extends Visualization {
 	/*
 	 * TO DO's
 	 * - Exception wenn man nur Import oder Export anwählt--> Warum?! (result ist dann null)
-	 * - Beschriftung!!
-	 * - Methode, die Land überprüft und dann den richtigen Namen gibt. 
+	 * - Beschriftung --> ev. Production fett und oben, Absätze
+	 * - Methode, die Land überprüft und dann den richtigen Namen ausgibt oder Datenbank änderen... 
 	 * 
 	 * */
 	String [][] IsoCodes = null;
@@ -76,25 +75,47 @@ public class GeoMapVisualization extends Visualization {
 		});	
 	}
 	
+	
+	@Override
+	public void draw() {
+		VisualizationUtils.loadVisualizationApi(
+				new Runnable() {
+					public void run() {
+					
+						if (config.getSelectedDataSeriesList().contains("1")) {
+							drawGeoMap("Production", productionresult);
+							
+						}
+						if (config.getSelectedDataSeriesList().contains("2")) {
+							drawGeoMap("Import", importresult);
+							
+						}
+						if (config.getSelectedDataSeriesList().contains("3")) {
+							drawGeoMap("Export", exportresult);
+							
+						}		
+					}	
+				}, AnnotatedTimeLine.PACKAGE, CoreChart.PACKAGE,
+				Gauge.PACKAGE, GeoMap.PACKAGE, ImageChart.PACKAGE,
+				ImageLineChart.PACKAGE, ImageAreaChart.PACKAGE, ImageBarChart.PACKAGE,
+				ImagePieChart.PACKAGE, IntensityMap.PACKAGE,
+				MapVisualization.PACKAGE, MotionChart.PACKAGE, OrgChart.PACKAGE,Table.PACKAGE,
+				ImageSparklineChart.PACKAGE);	
+	}
+	
+
 	private void drawGeoMap(String dataSerie, String result[][]){
-		for (int i = 0; i< IsoCodes.length; i++){
-			for (int j = 0; j< IsoCodes[0].length; j++){
-				System.out.println(IsoCodes[i][j]);
-			}
-		}
 		//create GeoMap
 		GeoMap.Options options = GeoMap.Options.create();
 		options.setRegion("world");
 		options.setShowLegend(true);
 		options.setSize(1000, 500);
 		
-
 		//add data to GeoMap
 		DataTable data = DataTable.create();
 		data.addColumn(ColumnType.STRING, dataSerie);
 		data.addColumn(ColumnType.NUMBER, dataSerie+ " per capita");
 		
-
 		int sumAllData = 0;
 		int counter = 0;
 		
@@ -128,63 +149,41 @@ public class GeoMapVisualization extends Visualization {
 				
 			}
 		}
+		
 		//gather selected years and items for the comment underneath the visualization
 		String allSelectedYears = "";
 		for (int i = 0; i<config.getSelectedYearsList().size(); i++){
-			if (i==(config.getSelectedYearsList().size()-1)){
-				allSelectedYears = allSelectedYears.concat(config.getSelectedYearsList().get(i));		
+			allSelectedYears = (allSelectedYears.concat(config.getSelectedYearsList().get(i)));	
+			if (i == config.getSelectedYearsList().size() -2){
+				allSelectedYears = allSelectedYears + " and ";
+			}
+			else if (i == config.getSelectedYearsList().size()-1){
+				allSelectedYears = allSelectedYears + " ";	
 			}else{
-			allSelectedYears = allSelectedYears.concat(config.getSelectedYearsList().get(i)) +", ";	
+				allSelectedYears = allSelectedYears + ", ";
+			}
 		}
+		String allSelectedItems = "";
+		for (int i = 0; i< config.getSelectedItemNameList().size(); i++){
+			allSelectedItems = (allSelectedItems.concat(config.getSelectedItemNameList().get(i)));	
+				if (i == config.getSelectedItemNameList().size() -2){
+					allSelectedItems = allSelectedItems + " and ";
+				}
+				else if (i == config.getSelectedItemNameList().size()-1){
+					allSelectedItems = allSelectedItems + " ";	
+				}else{
+					allSelectedItems = allSelectedItems + ", ";
+				}
 		}
-		String allSelectedItems = result[0][2]+ " ";
-		for (int i = 1; i< result.length; i++){
-			if (result[i][2].equals(result[i-1][2])==false){
-			allSelectedItems = (allSelectedItems.concat(result[i][2])) + ", ";	
-		}
-		}
-		TextArea text = new TextArea();
-		text.removeStyleName("TextArea");//doesn't function yet
-		text.addStyleName("TextAreaNew");//doesn't function yet
-		text.setReadOnly(true);
-		text.setPixelSize(430, 30);
-		text.setText(dataSerie+ " in tonnes divided through population" + " of " + allSelectedItems + "in " + allSelectedYears + ".");
-		GeoMap widget = new GeoMap(data, options);
-		
-		RootPanel.get("visualizationContainer").add(widget);
-		RootPanel.get("visualizationContainer").add(text);
-		
-	}
+		//HTML space = new HTML("");
+		HTML text = new HTML(dataSerie+ " per capita" +
+		" of " + allSelectedItems + "in " + allSelectedYears + ":");
 
-	@Override
-	public void draw() {
-		VisualizationUtils.loadVisualizationApi(
-				new Runnable() {
-					public void run() {
-						
-						
-						if (config.getSelectedDataSeriesList().contains("1")) {
-							drawGeoMap("Production", productionresult);
-							
-						}
-						if (config.getSelectedDataSeriesList().contains("2")) {
-							drawGeoMap("Import", importresult);
-							
-						}
-						if (config.getSelectedDataSeriesList().contains("3")) {
-							drawGeoMap("Export", exportresult);
-							
-						}
-						
-						
-					}
-					
-				}, AnnotatedTimeLine.PACKAGE, CoreChart.PACKAGE,
-				Gauge.PACKAGE, GeoMap.PACKAGE, ImageChart.PACKAGE,
-				ImageLineChart.PACKAGE, ImageAreaChart.PACKAGE, ImageBarChart.PACKAGE,
-				ImagePieChart.PACKAGE, IntensityMap.PACKAGE,
-				MapVisualization.PACKAGE, MotionChart.PACKAGE, OrgChart.PACKAGE,Table.PACKAGE,
-				ImageSparklineChart.PACKAGE);		
-	
+		GeoMap widget = new GeoMap(data, options);
+		//RootPanel.get("visualizationContainer").add(space);
+		RootPanel.get("visualizationContainer").add(text);
+		//RootPanel.get("visualizationContainer").add(space);
+		RootPanel.get("visualizationContainer").add(widget);
+		
 	}
 }
