9cfa8b8a765b25e8f93209a8f11645ba6bc58521
Nadine <nadine.piv@gmail.com>
2014-12-02 21:25:46 +0100

    Test AddTitles in Configuration

--- test/com/example/nudelvisualization/client/ConfigurationTest.java
+++ test/com/example/nudelvisualization/client/ConfigurationTest.java
@@ -7,7 +7,16 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class ConfigurationTest {
+	private ArrayList<String> selectedArea = new ArrayList<>();
+	private ArrayList<String> selectedAreaName = new ArrayList<>();
+	private ArrayList<String> selectedItems = new ArrayList<String>();
+	private ArrayList<String> selctedItemNames = new ArrayList<String>();
+	private ArrayList<String> selectedYears = new ArrayList<String>();
+	private ArrayList<String> selectedDataSeries = new ArrayList<String>();
+	private ArrayList<String> selectedDataSeriesName = new ArrayList<String>();
+	private ArrayList<String> titles = new ArrayList<String>();
 
+	
 	@Test
 	public void testConfiguration() {
 		Configuration tester = new Configuration();
@@ -19,6 +28,18 @@ public class ConfigurationTest {
 		assertEquals("1", tester.getSelectedDataSeriesList().get(0));
 		tester.addYear("1990");
 		assertEquals("1990", tester.getSelectedYearsList().get(0));
+	}
+	
+	@Test
+	public void testaddTitles(){
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("test");
+		Configuration config = new Configuration();
+		config.addTitles("test");
+		
+		assertEquals(test,config.getSelectedTitles());
+		
+		
 	}
 
 }
