c5c8658ea4a4e06de3a849eaea9d8dccaec18db6
Nadine <nadine.piv@gmail.com>
2014-12-02 21:43:54 +0100

    Controller Test

---
+++ test/com/example/nudelvisualization/client/ControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.nudelvisualization.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+	/**
+	 * @author nadinepiveteau
+	 * No J Unit test possible due to the fact that it is dependent on the GUI. No way to go around it. 
+	 */
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
